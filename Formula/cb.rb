004c3eecb1dd64633fa912926a43edced8e8fa8b
Will Leinweber <will@bitfission.com>
2021-07-02 17:53:17 -0700

    cb v0.3.1 (new formula)
    Closes #1.
    
    Signed-off-by: Will Leinweber <1973+will@users.noreply.github.com>

---
+++ Formula/cb.rb
@@ -0,0 +1,57 @@
+class Cb < Formula
+  desc "CLI for Crunchy Bridge"
+  homepage "https://crunchybridge.com"
+  url "https://github.com/will/cb/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "6be32df8dd65c4a69a4558eafdb52bac8986e06e88366fc4fcffb9d13f37badd"
+  license "GPL-3.0-or-later"
+
+  head do
+    url "https://github.com/will/cb.git"
+  end
+
+  depends_on "make" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "libevent"
+  depends_on "openssl@1.1"
+  depends_on "pcre"
+
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "bdw-gc"
+    depends_on "crystal" => :build
+  end
+
+  resource "crystal" do
+    on_linux do
+      url "https://github.com/crystal-lang/crystal/releases/download/1.0.0/crystal-1.0.0-1-linux-x86_64.tar.gz"
+      version "1.0.0-1"
+      sha256 "00211ca77758e99210ec40b8c5517b086d2ff9909e089400f6d847a95e5689a4"
+    end
+  end
+
+  def install
+    unless OS.mac?
+      # Use static Crystal compiler, since the one in Homebrew seems to be broken
+      # for Linux
+      (buildpath / "crystal").install resource("crystal")
+      ENV.prepend_path "PATH", "crystal/bin"
+      ENV.prepend_path "PATH", "crystal/embedded/bin"
+
+      ENV.prepend_path "PKG_CONFIG_PATH", (Formula["readline"].opt_lib / "pkgconfig")
+      ENV.prepend_path "PKG_CONFIG_PATH", (Formula["zlib"].opt_lib / "pkgconfig")
+
+      ENV.prepend_path "CRYSTAL_LIBRARY_PATH", (buildpath / "crystal/lib/crystal/lib")
+    end
+    # system "shards", "build", "--release"
+    system "make", "build", "RELEASE=1"
+
+    bin.install "bin/cb"
+    fish_completion.install "completions/cb.fish"
+  end
+
+  test do
+    assert_match "Usage: cb", shell_output(bin / "cb --help")
+  end
+end
